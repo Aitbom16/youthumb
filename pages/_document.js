f90529674351795df48bcd48ed31fb315664f492
Your NameAitbom16 <aitboumhaoutmed@gmail.com>
2023-10-16 10:57:17 +0000

    text

--- pages/_document.js
+++ pages/_document.js
@@ -1,49 +1,57 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang="en">
+            <Html lang="en-us">
                 <Head>
-                    {/* Add Google AdSense code to the head section */}
-                    <script
-                        async
-                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                    ></script>
+                    {/* Add your meta tags, stylesheets, and scripts here */}
+                    {/* ... */}
+
+                    {/* Add Google AdSense script */}
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
                 (adsbygoogle = window.adsbygoogle || []).push({
-                  google_ad_client: "ca-pub-1234567890", // Replace with your AdSense client ID
+                  google_ad_client: "ca-pub-5342472284328375",  // Your AdSense client ID
                   enable_page_level_ads: true
                 });
               `,
                         }}
                     />
-                    {/* End of Google AdSense code */}
-
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/favicon/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/favicon/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/favicon/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="/favicon/site.webmanifest" />
                 </Head>
                 <body>
+                    <nav className="nav">
+                        {/* ... */}
+                    </nav>
                     <Main />
                     <NextScript />
+                    <footer>
+                        <span>{/* ... */}</span>
+                    </footer>
+
+                    {/* Your provided content */}
+                    <div>
+                        <p>
+                            Use YouTubeThumbleDownload.com to quickly and easily download YouTube thumbnails. Save thumbnails of YouTube videos Get excellent thumbnail photos in Full HD (1080), HD (720), SD, and tiny sizes from any YouTube video. YouTube videos (HD, HQ, 1080p, and 4K) are supported formats.
+                        </p>
+                        <p>
+                            Why use YouTubeThambleDownload.com's YouTube Thumbnail Downloader? Use this YouTube thumbnail downloader to take screenshots from YouTube videos and use them for animations, presentations, and other projects.
+                        </p>
+                        <p>
+                            How Do I Use the Downloader for YouTube Thumbnails? To use this YouTube downloader step-by-step, refer to the screenshots that are provided. Copying the YouTube video link of the desired thumbnail to download is an easy operation. You may also download YouTube covers with ease.
+                        </p>
+                        {/* Add the rest of your content */}
+                        {/* ... */}
+                        <p>
+                            Visit youtubethambledownload.com to easily explore the potential of YouTube thumbnails. Download YouTube video thumbnails with ease to improve your presentations, elevate your content, and establish your online presence.
+                        </p>
+                        <p>
+                            Download YouTube Video Thumbnails Visit YouTubeThumbnailDownloader.com - Copyright 2023
+                        </p>
+                    </div>
                 </body>
             </Html>
         );
