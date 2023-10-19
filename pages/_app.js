c9e8dbcde83c15f2340f4e82d3a57a3d33873cbe
Your NameAitbom16 <aitboumhaoutmed@gmail.com>
2023-10-19 12:50:11 +0000

    Added text to MyComponent

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,12 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       <DefaultSeo
         title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
+              description="Get Free High-Quality YouTube Thumbnail Images! Our simple application allows you to download thumbnail images of varying quality. Simply enter the thumbnail URL of the video below and click 'Get Thumbnail Image.' Begin now to easily access your desired thumbnails!"
               canonical="https://www.youtubethambledownload.com/"
         openGraph={{
             url: "https://www.youtubethambledownload.com/",
           title: "Youtube Thumbnail Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
+            description: "Get Free High-Quality YouTube Thumbnail Images! Our simple application allows you to download thumbnail images of varying quality. Simply enter the thumbnail URL of the video below and click 'Get Thumbnail Image.' Begin now to easily access your desired thumbnails!",
           site_name: "Youtube Thumbnail Downloader",
         }}
       />
